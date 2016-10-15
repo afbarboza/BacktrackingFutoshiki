a284d9df1cb874137ef64d8aa7f4d89c409e0262
Jefferson Boldrin Cardozo <jefferson.boldrin@gmail.com>
2016-10-15 05:27:58 -0300

    Solução funncionando sem restrição de maior/menor

--- futoshiki.h
+++ futoshiki.h
@@ -1,52 +1,28 @@
 #ifndef FUTOSHIKI_H_INCLUDED
 #define FUTOSHIKI_H_INCLUDED
 
-#include <cstdint>
-#include <cstdlib>
-
-/*
-    number_case_test - numero de casos de teste do jogo
- */
+// number_case_test - numero de casos de teste do jogo
 int number_case_test = 0;
 
-/*
-    size_current_test - dimensao do caso de teste atual
-*/
+// size_current_test - dimensao do caso de teste atual
 int size_current_test = 0;
 
-/*
-   number_current_restrictions - numero de restricoes
-   para o caso de teste atual
- */
+// number_current_restrictions - numero de restricoes para o caso de teste atual
 int number_current_restrictions = 0;
 
-/*
-    play_matrix - representa o tabuleiro do jogo
- */
-uint8_t **play_matrix   = NULL;
+// play_matrix - representa o tabuleiro do jogo
+int **play_matrix   = NULL;
 
-/*
-    lines_map - dado um numero, mapeia em quais
-                linhas aquele numero  ja foi usado.
- */
+// lines_map - dado um numero, mapeia em quais linhas aquele numero ja foi usado.
 bool **lines_map        = NULL;
 
-/*
-    row_map -   dado um numero, mapeia em quais
-                colunas aquele numero ja foi usado.
- */
+// row_map - dado um numero, mapeia em quais colunas aquele numero ja foi usado.
 bool **columns_map      = NULL;
 
-/*
-    line_restriction - representa as restricoes
-                        de menor presentes nas linhas
-*/
-bool **line_restriction = NULL;
+// line_restriction - representa as restricoes de menor presentes nas linhas
+int **line_restriction = NULL;
 
-/*
-    columns_restriction - representa as restricoes
-                            de menor presentes nas colunas
-*/
-bool **columns_restriction = NULL;
+// columns_restriction - representa as restricoes de menor presentes nas colunas
+int **columns_restriction = NULL;
 
 #endif // FUTOSHIKI_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 extern int number_case_test;
 extern int size_current_test;
-extern uint8_t **play_matrix;
+
+extern int  **play_matrix;
+extern bool **lines_map;
+extern bool **columns_map;
+extern int  **line_restriction;
+extern int  **columns_restriction;
+
+
+bool SolveFutoshiki(int x, int y);
 
 int main()
 {
@@ -13,19 +21,115 @@ int main()
     while (number_case_test--) {
         get_size_test();
         get_number_restriction();
+
+        init_lines_map();
+        init_columns_map();
+
         init_play_matrix();
+        init_restrictions_map();
+
+        printf("Estado inicial:\n");
+        printf("Tabuleiro:\n");
         for (int i = 0; i < size_current_test; i++) {
             for (int j = 0; j < size_current_test; j++) {
                 printf("%d \t", play_matrix[i][j]);
             }
             printf("\n");
         }
+
+        printf("\nMapa de linhas:\n");
+        for (int i = 0; i < size_current_test; i++) {
+            for (int j = 0; j < size_current_test; j++) {
+                printf("%d \t", lines_map[i][j]? 1 : 0);
+            }
+            printf("\n");
+        }
+
+        printf("\nMapa de colunas:\n");
+        for (int i = 0; i < size_current_test; i++) {
+            for (int j = 0; j < size_current_test; j++) {
+                printf("%d \t", columns_map[i][j]? 1 : 0);
+            }
+            printf("\n");
+        }
+
+        printf("\nRestrições de linhas:\n");
+        for (int i = 0; i < size_current_test; i++) {
+            for (int j = 0; j < size_current_test; j++) {
+                printf("%d \t", line_restriction[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nRestrições de colunas:\n");
+        for (int i = 0; i < size_current_test; i++) {
+            for (int j = 0; j < size_current_test; j++) {
+                printf("%d \t", columns_restriction[i][j]);
+            }
+            printf("\n");
+        }
+
         getchar();
-        init_restrictions_map();
-        init_lines_map();
-        init_columns_map();
+
+        SolveFutoshiki(0,0);
+
         free_data();
     }
     return 0;
 }
 ;
+
+bool SolveFutoshiki(int x, int y){
+    printf("Estado atual: (analisando (%d, %d) )\n", x, y);
+    for (int i = 0; i < size_current_test; i++) {
+        for (int j = 0; j < size_current_test; j++) {
+            printf("%d \t", play_matrix[i][j]);
+        }
+        printf("\n");
+    }
+    getchar();
+
+    // Se foi até a última casa e tentou executar na linha de baixo significa que o jogo terminou com sucesso
+    if (y == size_current_test) return true;
+
+    // Se casa atual já está no tabuleiro inicial pula para a próxima
+    if (play_matrix[y][x] != 0){
+        if (x == (size_current_test -1))    return SolveFutoshiki(0, y + 1);
+        else                                return SolveFutoshiki(x + 1, y);
+    }
+
+    // Procura número que caiba na casa atual, indo de 1 a 'D'
+    bool result = false;
+    for (int i = 1; i <= size_current_test; i++){
+        if (!lines_map[i-1][y] && !columns_map[i-1][x]) {
+            play_matrix[y][x]   = i;
+            lines_map  [i-1][y] = true;
+            columns_map[i-1][x] = true;
+
+            if (x == (size_current_test -1))    result = SolveFutoshiki(0, y + 1);
+            else                                result = SolveFutoshiki(x + 1, y);
+
+            if (!result){
+                lines_map  [(play_matrix[y][x]) -1][y] = false;
+                columns_map[(play_matrix[y][x]) -1][x] = false;
+            }
+        }
+        // Se achou resultado true, retorna, senão segue o loop até testar todos os números
+        if (result) return true;
+    }
+    // Se chegou até aqui então não achou nenhum número, e a disposição atual do tabuleiro não possui solução.
+    // Assim, limpa a casa atual, reseta as variáveis correspondentes e retorna falso
+
+    play_matrix[y][x] = 0;
+
+    printf("Estado atual: (analisando (%d, %d): %d)\n", x, y, play_matrix[y][x]);
+    for (int i = 0; i < size_current_test; i++) {
+        for (int j = 0; j < size_current_test; j++) {
+            printf("%d \t", play_matrix[i][j]);
+        }
+        printf("\n");
+    }
+    getchar();
+
+    return false;
+}
